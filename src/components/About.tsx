578756c2572cd2510fe8457c934add42a20d9223
ismail hamdach <ismailhamdach@gmail.com>
2023-10-11 16:00:34 +0100

    Add responsivity to galery

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Image from "next/image";
 import { Link } from "react-scroll";
-import NextLink from "next/link";
 
 import Divider from "./Divider";
 import { Field, Form, Formik } from "formik";
--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -6,12 +6,12 @@ import Divider from "./Divider";
 const Galery = () => {
   return (
     <>
-      <div className="hidden lg:flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center min-h-screen pt-10">
         <p className=" text-3xl leading-8 font-bold tracking-tight text-text sm:text-4xl  pb-3">
           Notre travail
         </p>
         <Divider center />
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
             <div key={i} className="relative h-60 w-80 brightness-50 hover:brightness-100 cursor-pointer transition-all">
               <Image
